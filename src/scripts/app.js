6c2a040dcf4b320e1909583547c10b4ca947c2ee
Yassouxx <142691458+Yassouxx@users.noreply.github.com>
2026-03-24 12:37:05 +0100

    faire apparaitre les années en html et js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -219,3 +219,15 @@ btnValider.addEventListener('click', function() {
 
 
 // --------------------- calcul data pour la partie annee -----------------------------------
+
+const listeDesAnnees = document.querySelector('.year__list');
+const tableauYear = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025];
+
+tableauYear.forEach(function(ligneDuTemps){
+
+    yearItem = document.createElement('li');
+    yearItem.classList.add("year__item");
+    yearItem.textContent = ligneDuTemps;
+
+    listeDesAnnees.appendChild(yearItem);
+})
