41125f359cd158c9e5065623507d12b45d048496
Yassouxx <142691458+Yassouxx@users.noreply.github.com>
2026-03-24 10:14:51 +0100

    ajout de ma partie js + correction du scss pour que ça marche (LOL. c'est très bancal)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,221 @@
-console.info('Hello world');
+//  en gros : le morceau ici sert à attribuer la classe show pour que le block actor s'affiche quand on tape sur acteur dans la nav 
+//  suffit de copier coller ce morceau 2 fois et de mofifier les valeurs pour avoir ça avec genre et année aussi
+
+
+// --------------------- menu -----------------------------------
+
+// acteur
+const navActor = document.querySelector(".selector__actor");
+const sectionActor = document.querySelector(".actor");
+
+function actor() {
+    sectionActor.classList.toggle("block--show");
+    navActor.classList.toggle("selector__actor--active")
+}
+
+navActor.addEventListener("click", actor);
+
+// genre
+const navGenre = document.querySelector(".selector__genre");
+const sectionGenre = document.querySelector(".genre");
+
+function genre() {
+    sectionGenre.classList.toggle("block--show");
+    navGenre.classList.toggle("selector__genre--active")
+}
+
+navGenre.addEventListener("click", genre);
+
+// year
+const navYear = document.querySelector(".selector__year");
+const sectionYear = document.querySelector(".year");
+
+function year() {
+    sectionYear.classList.toggle("block--show");
+    navYear.classList.toggle("selector__year--active")
+}
+
+navYear.addEventListener("click", year);
+
+
+
+
+// --------------------- calcul data pour la partie acteur -----------------------------------
+
+// --------- 1 : recup les données json 
+
+let allActors = [];
+//  la j'ai fait un tableau pour mettre tous les noms des acteurs qui auront été recup avec le json dcp
+
+let allFilms = [];
+
+
+fetch ('assets/data.json')
+//  la dcp on charge le fichier json 
+.then(function(response){
+    return response.json();
+})
+.then(function(json){
+
+    allFilms = json;
+
+    json.forEach(function(film){
+        film.acteur.forEach(function(myActor){
+            if (myActor.nom && !allActors.includes(myActor.nom)){
+
+            
+                // le ! c'est pour dire l'inverse, donc if not includes le nom => alors on push le nom de l'acteur
+                allActors.push(myActor.nom);
+                console.log(allActors)
+
+            }
+        })
+    })
+});
+
+
+// la barre de recherche a proprement parler :
+// 1- On sélectionne ta barre de recherche (l'élément <input>) en JavaScript.
+// 2- On lui dit : "Dès que l'utilisateur appuie sur une touche (input), lance une fonction".
+// 3- Dans cette fonction, on regarde ce qui est écrit et on filtre notre liste d'acteur
+
+// je recupere la liste html 
+const sugList = document.getElementById('suggestions');
+// je cherche mon input via l'id 
+const search = document.getElementById('search-actor');
+
+search.addEventListener('input', function(){
+
+    // je recup ce que l'utilisateur ecrit
+    let userSearch = search.value.toLowerCase();
+
+    // ça c'est pr nettoyer la liste 
+    sugList.innerHTML = "";
+
+    // filtrer la liste pr garder que les acteurs qui ont les mêmes lettres 
+    let suggestions = allActors.filter(function(nom) {
+        console.log(nom);
+        return nom.toLowerCase().includes(userSearch);
+    });
+
+    if (userSearch.length > 2) {
+        console.log(suggestions);
+
+        suggestions.forEach(function(proposition){
+            let laProposition = document.createElement('li')
+            laProposition.textContent = proposition
+            laProposition.addEventListener('click', function(autoSearch){
+                search.value = proposition ;
+            })
+
+            sugList.appendChild(laProposition);
+            // en gros ça me crée un li a chaque fois que j'ai UNE proposition dans ma liste de suggestions
+            // suggestions -> c'est un paqut de carte trié en petit paquet 
+            // ma fonction propistion et donc le tableau laProposition -> une carte par li dans le paquet déjà trié
+        })
+    
+    }
+})
+
+
+
+
+// --------------------------- PARTIE AFFICHAGE
+
+
+// click sur valider -> filtre les données pour que le js reprenne tous les films ou les acteurs sont là 
+
+
+const btnValider = document.querySelector('.btn__valider')
+
+btnValider.addEventListener('click', function() {
+
+    nomChoisi = search.value ;
+
+    let filmsActeur = allFilms.filter(function(film){
+        return film.acteur.some(function(acteur){
+            // some = en gros au moins 1 des acteurs correspond a l'acteur choisi 
+
+            return acteur.nom === nomChoisi
+        });
+    });
+
+    //  la le but ça va etre de compté les données années et genre 
+
+    statsAnnees = {};
+    statsGenres = {};
+
+    filmsActeur.forEach(function(donnees){
+
+        // ----------- la c'est la part 1 les années 
+
+        let annee = donnees.date;
+
+        if(statsAnnees[annee]){
+            statsAnnees[annee] = statsAnnees[annee] + 1;
+        }else{
+            statsAnnees[annee] = 1;
+        };
+
+        
+
+        // ------------ part 2 les genres
+
+        donnees.genre.forEach(function(unGenre){
+
+            if(statsGenres[unGenre]){
+                statsGenres[unGenre] = statsGenres[unGenre] + 1;
+            }else{
+                statsGenres[unGenre] = 1;
+            }
+
+            console.log(statsGenres);
+
+        });
+        
+    });
+
+    // --------------- part 3 : transfo des deux stats en tableau pr faciliter l'affichage apres
+
+    let tableauAnnees = Object.entries(statsAnnees);
+    let tableauGenres = Object.entries(statsGenres);
+
+    let topDates = tableauAnnees.slice(0, 4);
+    let topGenres = tableauGenres.slice(0, 4);
+
+    const infoGenres = document.querySelector('#section-acteur .result-block--opposite .result-block__list');
+    infoGenres.innerHTML = "";
+
+    const infoDates = document.querySelector('#section-acteur .result-block--line .result-block__list');
+    infoDates.innerHTML = "";
+
+    let maxScoreGenre = topGenres[0][1];
+    let maxScoreDate = topDates[0][1];
+
+    infoGenres.innerHTML = "";
+    topGenres.forEach(function(unGenre){
+        const pourcentage = (unGenre[1] / maxScoreGenre * 100);
+        infoGenres.innerHTML += `
+            <div class="result-block__item stat-item" style="--progress: ${pourcentage}%">
+                <span class="stat-name">${unGenre[0]}</span>
+                <span class="stat-count">${unGenre[1]}</span>
+            </div>
+        `;
+
+    });
+
+    infoDates.innerHTML = "";
+    topDates.forEach(function(uneDate) {
+        const pourcentage = (uneDate[1] / maxScoreDate * 100);
+        infoDates.innerHTML += `
+            <div class="result-block__item stat-item" style="--progress: ${pourcentage}%">
+                <span class="stat-name">${uneDate[0]}</span>
+                <span class="stat-count">${uneDate[1]}</span>
+            </div>
+        `;
+    });
+
+});
+
+
+// --------------------- calcul data pour la partie annee -----------------------------------
